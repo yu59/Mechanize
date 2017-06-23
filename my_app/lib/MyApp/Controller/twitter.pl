6e08d9b73a36eb46bbf132e14f193f44af3dea00
yu tobaru <e145759@gmail.com>
2017-06-23 17:03:26 +0900

    fix twitter.pl

--- my_app/lib/MyApp/Controller/twitter.pl
+++ my_app/lib/MyApp/Controller/twitter.pl
@@ -1,17 +1,18 @@
 #!/usr/env/perl perl
 use strict;
 use warnings;
+use WWW::Mechanize;
 use LWP::Protocol::https;
 
-use WWW::Mechanize;
 my $mech = new WWW::Mechanize( autocheck => 1 );
 
 
 # $mech->get("https://twitter.com/");
 $mech->get('https://twitter.com/search-advanced?lang=ja');
 
-$mech->submit_form(
-	field
+my $result = $mech->submit_form(
+	fields => { ands => 'YAPC'}
 );
+print $mech->content;
 
 
